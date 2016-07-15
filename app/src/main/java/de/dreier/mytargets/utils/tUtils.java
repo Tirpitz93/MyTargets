3576b07c0817b3b53b14862f291248ca82a28ba0
lonja Selter <Loenja@gmail.com>
2016-07-15 22:31:48 +0200

    #4 pt 1, begin refactor passe to end

---
+++ app/src/main/java/de/dreier/mytargets/utils/tUtils.java
@@ -0,0 +1,13 @@
+package de.dreier.mytargets.utils;
+
+import android.util.Log;
+
+/**
+ * Created by loenj on 14/07/2016.
+ */
+public class tUtils {
+
+    public static void dl(String msg){
+        Log.d("Tirpitz",msg);
+    }
+}
